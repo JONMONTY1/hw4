a5f0e4728f33484bef8a61d28a6f923e6d699536
Jonathan Monty <jonathan.monty@kellogg.northwestern.edu>
2023-06-03 18:21:36 +0000

    commit1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+     if @user["password"] == params["password"]
+        # login the user
+        flash["notice"] = "Welcome"
+        redirect_to "/places"
+      else
+        redirect_to "/sessions/new"
+      end
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
   end
+
 end
-  
